ad4e88b31959598c0368c84a6441358ccfb11147
EldritchBear <>
2022-05-05 23:43:20 +0000

    new try

--- heap.c
+++ heap.c
@@ -53,7 +53,7 @@ void heap_push(Heap* pq, void* data, int priority){
 
 void heap_pop(Heap* pq){
   int i = 0;
-  heapElem *aux;
+  heapElem aux;
   pq->heapArray[0] = pq->heapArray[pq->size];
   pq->heapArray[pq->size].data = NULL;
   pq->heapArray[pq->size].priority = 0;
