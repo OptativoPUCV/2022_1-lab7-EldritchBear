92b9718533f5a482e6b78a4d1ee707732bb82c7d
EldritchBear <>
2022-05-05 15:49:23 +0000

    new try

--- heap.c
+++ heap.c
@@ -34,7 +34,7 @@ void heap_push(Heap* pq, void* data, int priority){
   pq->heapArray = realloc(pq->heapArray,pq->capac);
 
   for(i = 0; i < pq->capac ; i++){
-    if(pq->heapArray[i].data == NULL)
+    if(pq->heapArray[i].priority == NULL)
     {
       pq->heapArray[i].data = data;
       pq->heapArray[i].priority = priority;
