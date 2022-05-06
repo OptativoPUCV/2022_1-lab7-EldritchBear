3636d22fd2a19b115ab3edd82d6929981ddea445
EldritchBear <>
2022-05-06 00:03:56 +0000

    new try

--- heap.c
+++ heap.c
@@ -36,9 +36,9 @@ void heap_push(Heap* pq, void* data, int priority){
   pq->heapArray[pq->size].priority = priority;
 
   while(pq->heapArray[i].priority > pq->heapArray[(i-1)/2].priority){
-    heapElem aux = pq->heapArray[(i-1)/2];
-    pq->heapArray[(i-1)/2] = pq->heapArray[i];
-    pq->heapArray[i] = aux;
+    aux = pq->heapArray[i];
+    pq->heapArray[i] = pq->heapArray[(i-1)/2];
+    pq->heapArray[(i-1)/2] = aux;
     i = (i-1)/2;
   }
   pq->size += 1;
